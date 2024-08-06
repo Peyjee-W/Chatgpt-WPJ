e4d2803655d5c0c62fc99b162a94a06a465648c2
Peyjee-W <peyjeewang@gmail.com>
2024-08-06 21:02:54 +0800

    Add files via upload

---
+++ src/components/SystemRoleSettings.tsx
@@ -0,0 +1,69 @@
+import { Show } from 'solid-js'
+import IconEnv from './icons/Env'
+import IconX from './icons/X'
+import type { Accessor, Setter } from 'solid-js'
+
+interface Props {
+  canEdit: Accessor<boolean>
+  systemRoleEditing: Accessor<boolean>
+  setSystemRoleEditing: Setter<boolean>
+  currentSystemRoleSettings: Accessor<string>
+  setCurrentSystemRoleSettings: Setter<string>
+}
+
+export default (props: Props) => {
+  let systemInputRef: HTMLTextAreaElement
+
+  const handleButtonClick = () => {
+    props.setCurrentSystemRoleSettings(systemInputRef.value)
+    props.setSystemRoleEditing(false)
+  }
+
+  return (
+    <div class="my-4">
+      <Show when={!props.systemRoleEditing()}>
+        <Show when={props.currentSystemRoleSettings()}>
+          <div>
+            <div class="fi gap-1 op-70 dark:op-80">
+              <Show when={props.canEdit()} fallback={<IconEnv />}>
+                <span onClick={() => props.setCurrentSystemRoleSettings('')} class="sys-edit-btn p-1 rd-50%" > <IconX /> </span>
+              </Show>
+              <span>设定性格: </span>
+            </div>
+            <div class="mt-1">
+              {props.currentSystemRoleSettings()}
+            </div>
+          </div>
+        </Show>
+        <Show when={!props.currentSystemRoleSettings() && props.canEdit()}>
+          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="sys-edit-btn">
+            <IconEnv />
+            <span>可以在这设定我的性格</span>
+          </span>
+        </Show>
+      </Show>
+      <Show when={props.systemRoleEditing() && props.canEdit()}>
+        <div>
+          <div class="fi gap-1 op-50 dark:op-60">
+            <IconEnv />
+            <span>性格是:</span>
+          </div>
+          <p class="my-2 leading-normal text-sm op-50 dark:op-60">你是一个干练的人，常常可以简洁地回答我的问题。</p>
+          <div>
+            <textarea
+              ref={systemInputRef!}
+              placeholder="You are a helpful assistant, answer as concisely as possible...."
+              autocomplete="off"
+              autofocus
+              rows="3"
+              gen-textarea
+            />
+          </div>
+          <button onClick={handleButtonClick} gen-slate-btn>
+            设定
+          </button>
+        </div>
+      </Show>
+    </div>
+  )
+}
---
+++ src/components/icons/Env.tsx
@@ -0,0 +1,9 @@
+export default () => {
+  return (
+    <div style={{
+      width: '1em',
+      height: '1em',
+      background: `url('https://i.postimg.cc/m24gywW9/image.png') no-repeat center center / contain`,
+    }} />
+  )
+}
